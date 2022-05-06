4df680948a0d5c349f550cd4a51eab8b9172dd5b
Yiannis Christodoulou <yiannis@web357.com>
2022-05-06 03:06:20 +0300

    add

--- public/index.php
+++ public/index.php
@@ -3,4 +3,6 @@ echo "<h1>OK test test test 123 456 789</h1>";
 echo "<p>another test</p>";
 echo "<p>another test</p>";
 echo "<p>another test</p>";
+echo "<p>another test</p>";
+echo "<p>another test</p>";
 echo date('Y-m-d');
